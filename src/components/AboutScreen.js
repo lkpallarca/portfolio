184cfd2baa2a90e362b084d3108bf5aee6622eaa
lkpallarca <kencepallarca@gmail.com>
2022-05-17 17:56:58 +0800

    update styles automagically

--- src/components/AboutScreen.js
+++ src/components/AboutScreen.js
@@ -11,12 +11,11 @@ export default function AboutScreen() {
           Hello! My name is Lance, and I enjoy being involved with things that live on the internet.
         </h3>
         <div>
-          I am a person who is passion driven.
           As a graduate of <a href='https://www.patts.edu.ph/' target='blank'>Aeronautical Engineering,</a> I have always been fascinated by the advancement of technology and afraid of being left behind.
           Because of this, I dived into the world of software engineering, and it turns out that I am a pretty darn fast learner! I love collaborations and I hope to be involved with more in the future.
         </div>
         <div>
-          Fast forward to today, I've had the opportunity to be taken under the wing of an awesome <a href='https://www.avionschool.com/' target='blank'>bootcamp</a>. My main focus these days is expanding my network while absorbing as much as I can from as many software devs as possible.
+          Fast forward to today, I've had the opportunity to be taken under the wing of an awesome <a href='https://www.avionschool.com/' target='blank'>bootcamp</a>. My main focus these days is learning more and absorbing as much as I can from all forms of education.
         </div> 
         <div>
           Here are some of the languages and tools that I have learned/am learning throughout my journey as an aspiring software developer.
