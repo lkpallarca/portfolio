0d324893cd6932b241821fb28263a7a5e68f698d
lkpallarca <kencepallarca@gmail.com>
2022-05-19 12:31:52 +0800

    update resume link

--- src/utils/resumeLink.js
+++ src/utils/resumeLink.js
@@ -1,3 +1,3 @@
 export default function getResumeLink() {
-  return 'https://drive.google.com/uc?export=download&id=17ZkyMZs8jgphpAiE5Ug0N_kDSxoEhKHG'
+  return 'https://drive.google.com/uc?export=download&id=1m3_59N-BCtmJukXzNl1j0HXH-tT7KWK2'
 }
