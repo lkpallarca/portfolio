e938c7712b7304f2b770d1816a9893ecb8eaa453
Lance Pallarca <kencepallarca@gmail.com>
2022-08-18 19:30:19 +0800

    replace resume link

--- src/utils/resumeLink.js
+++ src/utils/resumeLink.js
@@ -1,3 +1,3 @@
 export default function getResumeLink() {
-  return 'https://drive.google.com/uc?export=download&id=1m3_59N-BCtmJukXzNl1j0HXH-tT7KWK2'
+  return 'https://drive.google.com/uc?export=download&id=1bhlWgeaJtgcp2wxl85B_h1hahaNOG7oa'
 }
